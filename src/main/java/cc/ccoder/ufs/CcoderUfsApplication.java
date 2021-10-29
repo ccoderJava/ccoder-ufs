f3c16212b109eb408d74a3e770306d305df40894
chencong <cong.chen@payby.com>
2021-10-29 18:20:30 +0800

    :tada: Initialize project structure

---
+++ src/main/java/cc/ccoder/ufs/CcoderUfsApplication.java
@@ -0,0 +1,13 @@
+package cc.ccoder.ufs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcoderUfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcoderUfsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/ccoder/ufs/CcoderUfsApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.ccoder.ufs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcoderUfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
