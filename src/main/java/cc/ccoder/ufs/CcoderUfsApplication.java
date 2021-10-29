6d76a6ad8cf9027cbfe2d81eb9f6f9621c4846b9
congcong <congccoder@gmail.com>
2021-10-29 18:24:14 +0800

    :tada: Initialize project structure

--- src/main/java/cc/ccoder/ufs/CcoderUfsApplication.java
+++ src/main/java/cc/ccoder/ufs/CcoderUfsApplication.java
@@ -3,6 +3,9 @@ package cc.ccoder.ufs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author chencong
+ */
 @SpringBootApplication
 public class CcoderUfsApplication {
 
